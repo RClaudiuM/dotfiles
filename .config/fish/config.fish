d85e354043ed087151a8c74e22c1e4f70e8fa586
Roman Claudiu <roman.claudiumihai@gmail.com>
2024-10-25 15:33:07 +0300

    Update fish config: Add aliases and yarn scripts, and open chrome with remote debug port

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,14 @@ set -U fish_user_paths /opt/homebrew/bin/ $fish_user_paths
 # fish_add_path /opt/homebrew/bin
 
 # starship init fish | source
+
+# Aliases
+alias c='clear'
+
+# Easier yarn scripts for react-nest monorepos
+alias ycsd="yarn catalog-api start:debug"
+alias yfd="yarn fe develop"
+alias ybsd="yarn be start:debug"
+
+# Open chrome with remote debug port
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222"
