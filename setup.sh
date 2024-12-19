88c9bc2a102770d8d0b6e8e0958499ceacf9fd98
Roman Claudiu <roman.claudiumihai@gmail.com>
2024-12-19 11:37:21 +0200

    Update gh hosts and stow configuration: Change default user and ignore setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install GNU Stow
+if ! command -v stow &> /dev/null
+then
+    echo "GNU Stow not found, installing..."
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install stow
+    else
+        sudo apt-get update
+        sudo apt-get install -y stow
+    fi
+else
+    echo "GNU Stow is already installed"
+fi
+
+# Run stow .
+stow .
