0ea6b18450828891e384f0e44624f155a10900b4
Roman Claudiu <roman.claudiumihai@gmail.com>
2024-12-20 15:18:28 +0200

    Update setup.sh: Add command_exists function and install fzf if not present on Linux

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
 # Install GNU Stow
 if ! command -v stow &> /dev/null
 then
@@ -12,6 +17,15 @@ then
     fi
 else
     echo "GNU Stow is already installed"
+fi
+
+# Install fzf if not present and on Linux
+if [[ "$OSTYPE" == "linux-gnu"* ]] && ! command_exists fzf; then
+    echo "Installing fzf..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+elif [[ "$OSTYPE" == "linux-gnu"* ]] && command_exists fzf; then
+    echo "fzf is already installed"
 fi
 
 # Remove existing .zshrc file
