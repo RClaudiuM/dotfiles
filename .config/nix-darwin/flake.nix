84382fd720e01bc91a2dd52ac69ae21e56359e5c
Roman Claudiu <roman.claudiumihai@gmail.com>
2025-03-21 11:18:20 +0200

    Update flake.nix and flake.lock: Change nix-homebrew URL to use Git and update Homebrew references

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -5,20 +5,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-homebrew.url = "git+https://github.com/zhaofengli/nix-homebrew?ref=refs/pull/71/merge";
 
     # Optional: Declarative tap management
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
-      flake = false;
+      # flake = false;
     };
     homebrew-cask = {
       url = "github:homebrew/homebrew-cask";
-      flake = false;
+      # flake = false;
     };
     homebrew-bundle = {
       url = "github:homebrew/homebrew-bundle";
-      flake = false;
+      # flake = false;
     };
   };
 
